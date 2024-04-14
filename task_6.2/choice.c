4f5e44a40f850d68f4f3e3ed6c4eba94a9422f08
juleeluc <juleeluc@studen.21-school.ru>
2024-04-15 00:17:13 +0700

    dyn_lib

---
+++ task_6.2/choice.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+void choice() {
+  printf("Menu:\n");
+  printf("1. Add contact\n");
+  printf("2. Delete contact\n");
+  printf("3. Show all contact\n");
+  printf("4. Edit contact\n");
+  printf("5. Find people\n");
+}
